12e42a1ecff0a819c54756a4c51a5aa7048b2ece
Jeremie <jeremiechiclote34@gmail.com>
2026-05-30 13:05:24 +0200

    Cambios

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,5 @@
 export const environment={
-  baseUrl:'http://localhost:3000',
+  //baseUrl:'http://localhost:3000', ---> Local
+  baseUrl:'https://real-g4-sneaker-v2-back-end.vercel.app', // ---> Back Live
   apiKey:'pk.eyJ1IjoiamVyZW1pZWtsb3NlMTIiLCJhIjoiY20zenJxbnA4MjN6dDJsczd6azFmejd0dyJ9.Q2EYBO7-L0O1rt1AdnJm8g'
 }
